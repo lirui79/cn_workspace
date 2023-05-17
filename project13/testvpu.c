6c93c631fbdffb8b5a853f394b0163471e7f174b
lirui <lirui4459@sina.com>
2023-05-17 16:17:29 +0800

    newfix: adjust source code On branch main Changes to be committed: 	modified:   project13/testvpu.c

--- project13/testvpu.c
+++ project13/testvpu.c
@@ -16,29 +16,29 @@
 
 int main(void)
 {
-    int fd, ret, i ;
+    int fd = -1, ret = -1, i = 0;
     char devName[32] = {0};
     char buffer[MM_SIZE] = {0};
     char* buf_addr = NULL;
     char version[32] = {0};
     for (i = 0; i < 4; ++i) {
-				sprintf(devName,"/dev/VPU%d", i);
-				fd = open(devName,O_RDWR);
-				unsigned int wvalue = 0x12323 + i, rvalue = 0;
-				buf_addr = mmap(NULL, MM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-				ret = read(fd, &rvalue, sizeof(unsigned int));
-				printf("read %d %x\n", ret, rvalue);
-				ret = write(fd, &wvalue, sizeof(unsigned int));
-				printf("write %d %x\n", ret, wvalue);
-            ret = ioctl(fd, VDRV_IOCTL_VERSION, (void*)version);
-            printf("version:%s\n", version);
-		    bzero(buffer, MM_SIZE);
-		    write(fd, buffer, MM_SIZE);
-		    write(fd, "write_device_mmap", strlen("write_device_mmap"));
-		    printf("write buffer: %s\n", buf_addr);
+		sprintf(devName,"/dev/VPU%d", i);
+		fd = open(devName,O_RDWR);
+		unsigned int wvalue = 0x12323 + i, rvalue = 0;
+		buf_addr = mmap(NULL, MM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		ret = read(fd, &rvalue, sizeof(unsigned int));
+		printf("read %d %x\n", ret, rvalue);
+		ret = write(fd, &wvalue, sizeof(unsigned int));
+		printf("write %d %x\n", ret, wvalue);
+		ret = ioctl(fd, VDRV_IOCTL_VERSION, (void*)version);
+		printf("version:%s\n", version);
+		bzero(buffer, MM_SIZE);
+		write(fd, buffer, MM_SIZE);
+		write(fd, "write_device_mmap", strlen("write_device_mmap"));
+		printf("write buffer: %s\n", buf_addr);
 
-		    munmap(buf_addr, MM_SIZE);
-				close(fd);
+		munmap(buf_addr, MM_SIZE);
+		close(fd);
     }
     
     return 0;
